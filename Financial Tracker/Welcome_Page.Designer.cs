c9fe135a750f791972914fe137a8f4f3f7bf9ae4
Tarek Alnagar <tarek.alnagar@outlook.com>
2026-04-07 13:31:49 +0200

    Add project files.

---
+++ Financial Tracker/Welcome_Page.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Financial_Tracker
+{
+    partial class Welcome_Page
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Welcome_Page
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.background;
+            ClientSize = new Size(1175, 659);
+            Name = "Welcome_Page";
+            Text = "Welcome Page";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ Financial Tracker/Welcome_Page.cs
@@ -0,0 +1,10 @@
+namespace Financial_Tracker
+{
+    public partial class Welcome_Page : Form
+    {
+        public Welcome_Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
