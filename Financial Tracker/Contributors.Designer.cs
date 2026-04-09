08835c9ad7833093fb5a43d689e966b9ef773406
Tarek Alnagar <tarek.alnagar@outlook.com>
2026-04-09 13:31:36 +0200

    .

---
+++ Financial Tracker/Contributors.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Financial_Tracker
+{
+    partial class Contributors
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(26, 37, 71);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(50, 45);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(392, 625);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Impact", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(12, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(174, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Contributors";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Impact", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(34, 162);
+            label2.Name = "label2";
+            label2.Size = new Size(295, 37);
+            label2.TabIndex = 1;
+            label2.Text = "Tarek Osama Alnagar ||";
+            // 
+            // Contributors
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(10, 15, 30);
+            ClientSize = new Size(503, 727);
+            Controls.Add(panel1);
+            Name = "Contributors";
+            Text = "Contributorscs";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label2;
+        private Label label1;
+    }
+}
